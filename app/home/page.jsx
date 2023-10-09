5bc0cf24ae378c525bcfe2aff5894bb9238756ef
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2023-10-08 18:53:49 -0600

    Sticky time range group on home page

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -5,27 +5,33 @@ import { TopTrack } from '@/components/TopTrack'
 import { TopTracks } from '@/components/TopTracks'
 import { TopArtists } from '@/components/TopArtists'
 import { TopGenres } from '@/components/TopGenres'
+import { TimeRangeGroup } from '@/components/TimeRangeGroup'
 
 export default function Home() {
     return (
-        <main className='w-full min-h-screen flex flex-col'>
-            <div className='grid grid-flow-row-dense grid-cols-1 sm:grid-cols-2 gap-2 md:gap-4'>
-                <div className='col-span-full'>
-                    <Header />
-                </div>
-                <RecentlyPlayed />
-                <CurrentlyPlaying />
-                <div className='col-span-full'>
-                    <TopTrack />
-                </div>
-                <div className='col-span-full'>
-                    <TopTracks />
-                </div>
-                <div className='col-span-full'>
-                    <TopArtists />
-                </div>
-                <div className='col-span-full'>
-                    <TopGenres />
+        <main className='w-full min-h-screen flex flex-col items-center p-6'>
+            <div className='relative max-w-5xl md:max-w-5xl xl:max-w-7xl'>
+                <div className='grid grid-flow-row-dense grid-cols-1 sm:grid-cols-2 gap-2 md:gap-4'>
+                    <section className='col-span-full'>
+                        <Header />
+                    </section>
+                    <section className='sticky top-[11%] sm:top-[13%] w-full flex justify-end bg-chinese-black col-span-full py-3 z-30'>
+                        <TimeRangeGroup />
+                    </section>
+                    <RecentlyPlayed />
+                    <CurrentlyPlaying />
+                    <section className='col-span-full'>
+                        <TopTrack />
+                    </section>
+                    <section className='col-span-full'>
+                        <TopTracks />
+                    </section>
+                    <section className='col-span-full'>
+                        <TopArtists />
+                    </section>
+                    <section className='col-span-full'>
+                        <TopGenres />
+                    </section>
                 </div>
             </div>
         </main>
